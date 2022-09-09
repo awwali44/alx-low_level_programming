e7fd9abec90111ea733409c7ca641317e3227600
Awwal ibrahim <awwaladebayo82@gmail.com>
2022-09-09 15:46:20 -0700

    multilingual

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
+
 /**
- *  * Description: main - Prints out a string to stdout.
- *   * Return: 0 if success.
- *    */
+ * main - prints the phrase "Programming is like building a multilingual puzzle
+ * Return: 0 if exited properly, non-zero otherwise
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
